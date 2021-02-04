a2eff8043b6297215ea1d552ab8c8cb15406fdb2
tacks9 <tacks321@qq.com>
2021-02-04 19:03:03 +0800

    go几种数据类型 字符串、整型、浮点型、布尔型

---
+++ example/02_date_types.go
@@ -0,0 +1,25 @@
+/*
+ * @Descripttion: go的几种值类型， 字符串、整型、浮点型、布尔型
+ * @Author: tacks321@qq.com
+ * @Date: 2021-02-04 18:53:14
+ * @LastEditTime: 2021-02-04 19:00:54
+ */
+
+package main
+
+import "fmt"
+
+func main() {
+
+	// 字符串
+	fmt.Println("go" + "lang")
+
+	// 整数
+	fmt.Println("1 + 1 = ", 1+1)
+
+	// 浮点数
+	fmt.Println("7.0 / 3.0 = ", 7.0/3.0)
+
+	// 布尔型
+	fmt.Println(true)
+}

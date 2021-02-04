c9ea44acc9d74c59963c43df7d63bf58a318d837
tacks9 <tacks321@qq.com>
2021-02-04 18:52:01 +0800

    01_HelloWorld

---
+++ example/01_hello_world.go
@@ -0,0 +1,21 @@
+/*
+ * @Descripttion: 打开新世界的大门 HelloWorld
+ * @Author: tacks321@qq.com
+ * @Date: 2021-02-04 18:34:20
+ * @LastEditTime: 2021-02-04 18:46:32
+ */
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World")
+}
+
+/*
+1、编译并执行
+	go run .\01_hello_world.go
+2、仅编译生成二进制文件.exe
+	go build .\01_hello_world.go
+*/
